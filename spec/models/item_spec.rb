9395ee43497dd69bb263e095601bd80f9d3f755b
Adam Maślanka <adam.maslanka+github@nsc-studio.com>
2022-07-13 20:02:15 +0200

    Add validations specs for item

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,4 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:name) }
+    it { should validate_presence_of(:price) }
+    it { should validate_presence_of(:amount) }
+  end
 end
