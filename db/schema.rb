9a3291a15f3aae54d4ac83158b0e176db08e1d95
Adam Maślanka <adam.maslanka+github@nsc-studio.com>
2022-07-14 14:35:25 +0200

    Add ingredients table

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_07_14_121009) do
+ActiveRecord::Schema[7.0].define(version: 2022_07_14_123305) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.bigint "item_id", null: false
+    t.decimal "quantity", default: "1.0", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_ingredients_on_item_id"
+    t.index ["product_id"], name: "index_ingredients_on_product_id"
+  end
 
   create_table "items", force: :cascade do |t|
     t.string "name", null: false
@@ -30,4 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_14_121009) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "ingredients", "items"
+  add_foreign_key "ingredients", "products"
 end
